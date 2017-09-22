1fff135af7f190e0b92cb3e6813784532ca2be63
LasaleFamine <info@godev.space>
2017-09-22 22:51:12 +0200

    [WIP] removed nyc and codecov for now and added tests

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,10 @@ import test from 'ava';
 import pify from 'pify';
 import helpers from 'yeoman-test';
 import assert from 'yeoman-assert';
-import {beforeEach, after} from './helpers';
+import {
+  beforeEach,
+  after
+} from './helpers';
 
 const PATH_APP_GEN = '../../generators/app';
 
@@ -11,21 +14,46 @@ test.after.always(after);
 
 test.serial('generates expected files', async () => {
   const generator = helpers.createGenerator(
-    'polymerx:app', [PATH_APP_GEN], null, {skipInstall: true}
+    'polymerx:app', [PATH_APP_GEN], null, {
+      skipInstall: true
+    }
   );
 
   helpers.mockPrompt(generator, {
     appName: 'test name',
     appDescription: 'some description',
     githubUsername: 'username',
-    website: 'some.website',
-    nyc: true,
-    codecov: true
+    website: 'some.website'
   });
 
   await pify(generator.run.bind(generator))();
 
   assert.file([
-    'webpack.config.js'
+    'webpack.config.js',
+    'package.json',
+    'postcss.config.js',
+    'wct.conf.js'
   ]);
+});
+
+test.serial('prompts for name, description, username, website, reponame', async () => {
+  const generator = helpers.createGenerator(
+    'polymerx:app', [PATH_APP_GEN], null, {
+      skipInstall: true
+    }
+  );
+
+  helpers.mockPrompt(generator, {
+    appName: 'test-name',
+    appDescription: 'foo',
+    githubUsername: 'username',
+    website: 'some.website'
+  });
+
+  await pify(generator.run.bind(generator))();
+
+  assert.fileContent('package.json', /"name": "test-name",/);
+  assert.fileContent('package.json', /"description": "foo",/);
+  assert.fileContent('package.json', /"repository": "username\/test-name",/);
+  // assert.fileContent('README.md', /> foo/);
 });
