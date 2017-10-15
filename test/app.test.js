7e32af0c23220864b7eadfb7035814aa92e0ea00
LasaleFamine <info@godev.space>
2017-10-15 15:10:10 +0200

    feat(test): added tests for app generator

--- test/app.test.js
+++ test/app.test.js
@@ -20,10 +20,7 @@ test.serial('generates expected files', async () => {
   );
 
   helpers.mockPrompt(generator, {
-    appName: 'test name',
-    appDescription: 'some description',
-    githubUsername: 'username',
-    website: 'some.website'
+    appName: 'test-name'
   });
 
   await pify(generator.run.bind(generator))();
@@ -32,8 +29,29 @@ test.serial('generates expected files', async () => {
     'webpack.config.js',
     'package.json',
     'postcss.config.js',
-    'wct.conf.js'
+    'wct.conf.js',
+    'webpack-module-build.config.js',
+    'README.md',
+    'LICENSE.md',
+    '.stylelintrc.json',
+    '.npmrc',
+    '.yarnrc',
+    '.editorconfig',
+    '.gitignore',
+    '.travis.yml',
+    'src/index.html',
+    'src/index.js',
+    'src/manifest.json',
+    'src/assets/',
+    'src/global-style',
+    'src/components/containers/test-name/index.js',
+    'src/components/containers/test-name/template.html',
+    'src/components/containers/test-name/style.postcss',
+    'src/components/dumbs/sk-menu',
+    'src/components/dumbs/sk-menu-item'
   ]);
+
+  assert.noFile(['src/components/containers/sk-app']);
 });
 
 test.serial('prompts for name, description, username, website, reponame', async () => {
@@ -55,5 +73,35 @@ test.serial('prompts for name, description, username, website, reponame', async 
   assert.fileContent('package.json', /"name": "test-name",/);
   assert.fileContent('package.json', /"description": "foo",/);
   assert.fileContent('package.json', /"repository": "username\/test-name",/);
-  // assert.fileContent('README.md', /> foo/);
+});
+
+test.serial('check content inside src', async () => {
+  const generator = helpers.createGenerator(
+    'polymerx:app', [PATH_APP_GEN], null, {
+      skipInstall: true
+    }
+  );
+
+  helpers.mockPrompt(generator, {
+    appName: 'awesome-app'
+  });
+
+  await pify(generator.run.bind(generator))();
+
+  assert.fileContent(
+    'src/components/containers/awesome-app/index.js',
+    /AwesomeApp extends PolymerElement/
+  );
+  assert.fileContent(
+    'src/components/containers/awesome-app/index.js',
+     /('awesome-app', AwesomeApp)/
+  );
+  assert.fileContent(
+    'src/index.html',
+    /<awesome-app unresolved name="awesome-app">/
+  );
+  assert.fileContent(
+    'src/index.html',
+    /<\/awesome-app>/
+  );
 });
